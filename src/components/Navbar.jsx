188730e59f89424c62b3177f164a7933bc061175
turnertop <turnerfool@pm.me>
2023-01-27 20:21:48 -0800

    added logo to navbar and added navigation links to navbar, need to learn mapping .map

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { useState } from 'react'
 
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
+
+
+// w-full sets full width
+// fuck it just look at tailwind docs/ cheatsheet
+// need to read docs on your own to become god otherwise will become frank
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+        <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+          {navLinks.map((nav, index) => (
+            <li 
+              key={nav.id} 
+              className={`font-poppins font-normal cursor-pointer text-[16px]${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}>
 
+              <a href={`#${nav.id}`}>
+                {nav.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+    </nav>
 
   );
 }
