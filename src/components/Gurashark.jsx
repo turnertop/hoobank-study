3c201a529f0c00cee593b59955b08499fd380e01
turnertop <turnerfool@pm.me>
2023-01-28 19:14:51 -0800

    gurashark

---
+++ src/components/Gurashark.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+// frankkkkkkkkkkkkkkkkkkkkkk where are youiuuuuuuuuuuuuuuuuuuu
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// jasper where are youuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
+//
+//
+//
+//
+// hehehehee!!!!!!1
+// some midget in my head told me to learn html css react
+//
+//
+//
+// some person in my head told me to learn turbopack nextjs vitejs kubernetes limbojs!!!1
+//
+//
+//
+//
+// look behind youuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
+export default function Gurashark() {
+ return (
+    <div className="suck my dick">
+      <h1 className="im bored as fuck so much shit too learn information overload!!!!">
+
+
+      </h1>
+    </div>
+
+
+
+ );
+}
+
+
+
+
+
