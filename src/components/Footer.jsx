ed107a53d070d11746387750933bff2dc21d06d0
turnertop <turnerfool@pm.me>
2023-01-29 20:52:20 -0800

    finished footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,10 +25,10 @@ const Footer = () => {
           {footerLinks.map((footerLink) => (
             <div key={footerLink.key} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
               <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">{footerLink.title}</h4>
-              <ul>
+              <ul className="list-none mt-4">
 
-                {footerLink.links.map((link) => (
-                  <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer`}>
+                {footerLink.links.map((link, index) => (
+                  <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerLink.links.length - 1 ? 'mb-4' : 'mb-0'}`}>
                   {link.name}
                   </li>
               ))}</ul>
@@ -41,6 +41,24 @@ const Footer = () => {
       </div>
 
       </div>
+
+
+      <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3r45]">
+        <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">2023 HooBank. All Rights Reserved.
+        </p>
+        <div className="flex flex-row md:mt-0 mt-6">
+          {socialMedia.map((social, index) => (
+        <img key={social.id}
+          src={social.icon}
+          alt={social.id}
+          className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0' }`}
+          />
+          ))}
+        </div>
+      </div>
+
+
+
     </section>
 
 
