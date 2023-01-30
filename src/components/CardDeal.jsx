bf3a83a5a79bd5389d219a820d00bd62985fe365
turnertop <turnerfool@pm.me>
2023-01-29 19:58:50 -0800

    finished card section

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { card } from '../assets';
+import styles, { layout } from '../style.js';
+import Button from './Button.jsx';
 
 const CardDeal = () => {
   return (
-    <div>
-      CardDeal
-    </div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden" /> in few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>It is funny that when they laugh, I'm not laughing. They don't realize I don't forget anything. When the chips are down and the constructs no longer protect them, I will have the last laugh. 
+        </p>
+        <Button styles="mt-10" />
+      </div>
+        
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-[100%] h-[100%]" />
+      </div>
+
+      </section>
 
 
   );
